3c07fa2a6a6ba75960cf7dc64f53aa45c5256200
Ricardo Franco <ricardo.krieg@gmail.com>
2016-04-14 18:16:56 -0300

    Update newsletter_subscriber.rb

--- app/models/spree/newsletter_subscriber.rb
+++ app/models/spree/newsletter_subscriber.rb
@@ -4,6 +4,6 @@ module Spree
     validates_uniqueness_of :email
     #validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
     
-    attr_accessible :email
+    #attr_accessible :email
   end
 end
